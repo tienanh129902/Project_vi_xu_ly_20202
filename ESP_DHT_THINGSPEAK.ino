07741fc4812d124709db069e8a6e7633f1674fe5
= <=>
2021-07-26 21:07:43 +0700

    1st commit

---
+++ ESP_DHT_THINGSPEAK.ino
@@ -0,0 +1,84 @@
+#include <DHT.h>
+#include <WiFi.h>
+#include <ThingSpeak.h>
+#include <Adafruit_Sensor.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+const char* ssid = "VNPT 2.4G";   // your network SSID (name)
+const char* password = "hoilamgi";   // your network password
+
+WiFiClient  client;
+
+unsigned long myChannelNumber = 1;
+const char * myWriteAPIKey = "PTXT65EGKSUIP6QJ";
+unsigned long lastTime = 0;
+unsigned long timerDelay = 30000;
+float f, t, h;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println(F("DHT11 test!"));
+  WiFi.mode(WIFI_STA);
+  dht.begin();
+  ThingSpeak.begin(client);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if ((millis() - lastTime) > timerDelay) {
+
+    // Connect or reconnect to WiFi
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.print("Attempting to connect");
+      while (WiFi.status() != WL_CONNECTED) {
+        WiFi.begin(ssid, password);
+        delay(5000);
+      }
+      Serial.println("\nConnected.");
+    }
+
+    // Wait a few seconds between measurements.
+    delay(2000);
+
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    t = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    f = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t) || isnan(f)) {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+    }
+
+    Serial.print(F("Humidity: "));
+    Serial.print(h);
+    Serial.print(F("%  Temperature: "));
+    Serial.print(t);
+    Serial.print(F("°C "));
+    Serial.print(f);
+    Serial.print(F("°F "));
+
+    // set the fields with the values
+    ThingSpeak.setField(1, t);
+    ThingSpeak.setField(2, h);
+    ThingSpeak.setField(3, f);
+    // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
+    // pieces of information in a channel.  Here, we write to field 1.
+    int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+    if (x == 200) {
+      Serial.println("Channel update successful.");
+    }
+    else {
+      Serial.println("Problem updating channel. HTTP error code " + String(x));
+    }
+    lastTime = millis();
+  }
+
+}
